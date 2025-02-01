1317dbcbc06b1530c60e945b805429ecccee9d10
demigod <divyanshjain.2206@gmail.com>
2025-02-01 16:44:50 +0530

    update token contract

--- contracts/mint_p3tf1_coin.sol
+++ contracts/mint_p3tf1_coin.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// 0x7244a8C2Ff9c1319fd08985A7CeDC47d82dB724C
+// 0x2258D27a3e0452D31A51c66f15C3C72EbD55eEB9
 
 pragma solidity ^0.8.20;
 
@@ -7,14 +7,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract P3TF1Token is ERC20, Ownable {
-    uint256 public constant RATE = 0.0001 ether;
+    uint256 public constant RATE = 0.000001 ether;
 
     constructor() ERC20("P3TF1 Coin", "P3TF1") Ownable(msg.sender) {
-        _mint(msg.sender, 1000000 * 10 ** decimals()); // Initial supply
+        _mint(msg.sender, 1000000 * 10 ** decimals());
     } 
 
     function buyTokens() external payable {
-        require(msg.value >= RATE, "Minimum purchase is 0.0001 ETH");
+        require(msg.value >= RATE, "Minimum purchase is 0.000001 ETH");
 
         uint256 tokensToMint = (msg.value / RATE) * 10 ** decimals();
         _mint(msg.sender, tokensToMint);
